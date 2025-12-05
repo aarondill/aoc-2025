9457624f0dae907e90966492cfa14f0572ee670b
Aaron Dill <aaronsacks2006@gmail.com>
2025-12-05 03:43:00 -0600

    feat: day1 part2

--- src/day1.rs
+++ src/day1.rs
@@ -26,3 +26,29 @@ pub fn part1(input: &str) -> usize {
         })
         .count()
 }
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> usize {
+    let mut dial: i32 = 50;
+    let mut count = 0;
+    for line in input.lines() {
+        let dir = line.chars().next().expect("Invalid input: no direction");
+        let dir: i8 = match dir {
+            'L' => -1,
+            'R' => 1,
+            _ => panic!("Invalid direction"),
+        };
+        let mag: i32 =
+            line.chars().skip(1).collect::<String>().parse().expect("Invalid input: not a number");
+        // Rust is fast enough that this actually works, despite the fact that this is a very slow method.
+        for _ in 0..mag {
+            dial = (dial + dir as i32) % 100;
+            if dial < 0 {
+                dial += 100;
+            }
+            if dial == 0 {
+                count += 1;
+            }
+        }
+    }
+    count
+}
