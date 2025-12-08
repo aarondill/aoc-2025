9af34cf757c577ec7e7a28244292bd95a6e05882
Aaron Dill <aaronsacks2006@gmail.com>
2025-12-08 03:09:44 -0600

    refactor: day1 to use a generator

--- src/day1.rs
+++ src/day1.rs
@@ -1,23 +1,33 @@
-#[aoc(day1, part1)]
-pub fn part1(input: &str) -> usize {
-    let mut dial: i32 = 50;
+type Rotation = i32;
+type Input = Vec<Rotation>;
+
+#[aoc_generator(day1)]
+pub fn parse(input: &str) -> Input {
     input
         .lines()
         // Yeah, yeah, mutation in a filter is bad, but it works.
-        .filter(|line| {
-            let dir = line.chars().next().expect("Invalid input: no direction");
-            let dir: i8 = match dir {
-                'L' => -1,
-                'R' => 1,
-                _ => panic!("Invalid direction"),
+        .map(|line| {
+            let mut iter = line.chars();
+            let dir = match iter.next() {
+                Some('L') => -1,
+                Some('R') => 1,
+                Some(_) => panic!("Invalid input: invalid direction"),
+                None => panic!("Invalid input: no direction"),
             };
-            let mag: i32 = line
-                .chars()
-                .skip(1)
-                .collect::<String>()
-                .parse()
-                .expect("Invalid input: not a number");
-            let change: i32 = (mag * dir as i32) % 100;
+            let mag: i32 = iter.collect::<String>().parse().expect("Invalid input: not a number");
+            mag * dir
+        })
+        .collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &Input) -> usize {
+    let mut dial = 50;
+    input
+        .iter()
+        // Yeah, yeah, mutation in a filter is bad, but it works.
+        .filter(|&&rotation| {
+            let change = rotation % 100;
             dial = (dial + change) % 100;
             if dial < 0 {
                 dial += 100;
@@ -27,21 +37,13 @@ pub fn part1(input: &str) -> usize {
         .count()
 }
 #[aoc(day1, part2)]
-pub fn part2(input: &str) -> usize {
-    let mut dial: i32 = 50;
+pub fn part2(input: &Input) -> usize {
+    let mut dial = 50;
     let mut count = 0;
-    for line in input.lines() {
-        let dir = line.chars().next().expect("Invalid input: no direction");
-        let dir: i8 = match dir {
-            'L' => -1,
-            'R' => 1,
-            _ => panic!("Invalid direction"),
-        };
-        let mag: i32 =
-            line.chars().skip(1).collect::<String>().parse().expect("Invalid input: not a number");
+    for rot in input {
         // Rust is fast enough that this actually works, despite the fact that this is a very slow method.
-        for _ in 0..mag {
-            dial = (dial + dir as i32) % 100;
+        for _ in 0..rot.abs() {
+            dial = (dial + rot.signum()) % 100;
             if dial < 0 {
                 dial += 100;
             }
@@ -59,10 +61,10 @@ mod tests {
     const INPUT: &str = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
     #[test]
     fn test_part1() {
-        assert_eq!(part1(INPUT), 4);
+        assert_eq!(part1(&parse(INPUT)), 3);
     }
     #[test]
     fn test_part2() {
-        assert_eq!(part2(INPUT), 6);
+        assert_eq!(part2(&parse(INPUT)), 6);
     }
 }
